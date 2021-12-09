a67c23940d1374af951cd22654f52936e6277fb9
Astro <astro@spaceboyz.net>
2021-12-09 18:16:25 +0100

    flake.nix: switch from mozillapkgs to fenix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,18 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
-    mozillapkgs.url = "github:mozilla/nixpkgs-mozilla";
-    mozillapkgs.flake = false;
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, mozillapkgs }:
+  outputs = { self, nixpkgs, utils, naersk, fenix }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages."${system}";
-      mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") {};
-      rust = (mozilla.rustChannelOf {
-        channel = "stable";
-        date = "2021-11-01";
-        sha256 = "1038ss84i5pf4zrgbgm3lyi5mvkycbm7fzd94ribf8ymagl8g71h";
-      }).rust;
+      rust = fenix.packages.${system}.stable.withComponents [
+        "cargo"
+        "rustc"
+      ];
 
       # Override the version used in naersk
       naersk-lib = naersk.lib."${system}".override {
